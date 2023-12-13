e1297214e6ce3c287121f2d4700ecc4e3b84fcf6
wikto <wikto@KOMPWIKI>
2023-12-13 13:54:44 +0100

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            string B = "Szkoła Dotneta";
+            float C = 12.5F;
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+           
+        }
+    }
+}
